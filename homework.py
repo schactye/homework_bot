11a57f21585c715d3d756aff031c79ff126fb8b9
“Елена <borovina@bk.ru>
2022-02-18 14:53:48 +0300

    «7»

--- homework.py
+++ homework.py
@@ -44,8 +44,7 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
-        response = request.Request.get(ENDPOINT, headers=HEADERS,
-                        params=params)
+        response = request.Request.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != 200:
             logger.error('No server response')
             raise
